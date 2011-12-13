41550392f7340e895019c43621f2d51da8439815
U-matt-PC\matt <matt@matt-PC.(none)>
2011-12-12 23:57:47 -0800

    Adding scripts

--- vmscripts/archlinux-1.sh
+++ vmscripts/archlinux-1.sh
@@ -1,11 +1,10 @@
 #second script
 pacman -Syu
-pacman -S --noconfirm sudo openssh python2 vim curl wget base-devel openssl
-echo "alias vi='vim'"
+pacman -S --noconfirm sudo openssh python2 vim curl base-devel openssl
+echo "alias vi='vim'" >> .bashrc
 rc.d start sshd
 vim /etc/rc.conf
 echo "%sudo ALL=(ALL) ALL" >> /etc/sudoers
 curl http://python-distribute.org/distribute_setup.py | python2
 curl https://raw.github.com/pypa/pip/master/contrib/get-pip.py | python
 pacman -S --noconfirm git
-git clone git
